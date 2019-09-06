427b306ad9b805ab68a1b3b7ceac3b38bbfb76ed
Janden.Ma <516113525@qq.com>
2019-09-07 03:57:04 +0800

    bug fixes: Can not resolve "../xlsx"

--- odsbits/10_utils.js
+++ odsbits/10_utils.js
@@ -1,10 +1,14 @@
 var get_utils = function() {
-	if(typeof XLSX !== 'undefined') return XLSX.utils;
-	if(typeof module !== "undefined" && typeof require !== 'undefined') try {
-		return require('../' + 'xlsx').utils;
-	} catch(e) {
-		try { return require('./' + 'xlsx').utils; }
-		catch(ee) { return require('xl' + 'sx').utils; }
-	}
-	throw new Error("Cannot find XLSX utils");
-};
+  if (typeof XLSX !== 'undefined') return XLSX.utils
+  if (typeof module !== 'undefined' && typeof require !== 'undefined')
+    try {
+      return require('./xlsx').utils
+    } catch (e) {
+      try {
+        return require('../xlsx').utils
+      } catch (ee) {
+        return require('xlsx').utils
+      }
+    }
+  throw new Error('Cannot find XLSX utils')
+}
